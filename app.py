8fe286d2da8bf0cbdf65782acc9b7257ae090b61
Ricardo Tasso <63082032+ricardotasso@users.noreply.github.com>
2024-06-12 16:55:26 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import flet as ft
+import requests
+
+def main(page: ft.Page):
+    page.title = "Random Dog Images"
+
+    def get_random_dog_image():
+        response = requests.get("https://picsum.photos/800/600")
+        return response.url
+
+    def show_random_dog_image(e):
+        image.src = get_random_dog_image()
+        page.update()
+
+    image = ft.Image(width=800, height=600)
+    button = ft.ElevatedButton("Get Random Dog Image", on_click=show_random_dog_image)
+
+    page.add(
+        ft.Column(
+            [
+                ft.Text("Click the button to get a random dog image:"),
+                image,
+                button,
+            ]
+        )
+    )
+
+ft.app(target=main)
